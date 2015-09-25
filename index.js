59c4938bbfb2a21376d73b0eaffe520eff99e6aa
Levi Donaldson <lcdbacanas@gmail.com>
2015-09-24 22:27:53 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+$(function () {
+    $('.mychart').highcharts({
+        chart: {
+            type: 'areaspline'
+        },
+        title: {
+            text: 'Recent Users'
+        },
+        // legend: {
+            // layout: 'vertical',
+            // align: 'left',
+            // verticalAlign: 'top',
+            // x: 150,
+            // y: 100,
+            // floating: true,
+            // borderWidth: 1,
+            // backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+        // },
+        // xAxis: {
+        //     categories: [
+        //         '1',
+        //         '2',
+        //         '3',
+        //         '4',
+        //         '5',
+        //         '6',
+        //         '7'
+        //     ],
+        //     plotBands: [{ // visualize the weekend
+        //         from: 4.5,
+        //         to: 6.5,
+        //         color: 'rgba(68, 170, 213, .2)'
+        //     }]
+        // },
+        yAxis: {
+            title: {
+                text: 'Signups Per Day'
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' units'
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series: [{
+            name: 'Users',
+            data: [3, 4, 3, 5, 4, 10, 12]    
+        }]
+    });
+});
